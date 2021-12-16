b277e358f58d5cec44664e47d6b30010c75a4489
duxmazter <46duxmazter@gmail.com>
2021-12-16 17:29:18 +0700

    fin

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    double sum;
+    int i = 0;
+    double insqrt;
+
+    ifstream source("score.txt");
+    string textline;
+
+    while (getline(source,textline))
+    {   
+        sum += stod(textline);
+        insqrt += pow(stod(textline),2);
+        i++;
+        // cout << textline << "\n";
+    }
+     
+    cout << "Number of data = "<< i << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/i << "\n";
+    cout << "Standard deviation = "<< sqrt((insqrt/i)-pow((sum/i),2));
+
+    return 0;
+}
